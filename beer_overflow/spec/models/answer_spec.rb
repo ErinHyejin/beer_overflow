2c9bb3a35fe341756f713314d53d5762d296cefe
Erin Hyejin Kim <lvhrhj@gmail.com>
2015-04-15 16:16:55 -0700

    change folders

--- beer_overflow/spec/models/answer_spec.rb
+++
@@ -1,7 +0,0 @@
-
-require 'rails_helper'
-
-RSpec.describe Answer, type: :model do
-  # expect(question).to have_many(:answers)
-  it { should belong_to (:question) }
-end
--- beer_overflow/spec/models/question_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Question, type: :model do
-  # expect(question).to have_many(:answers)
-  it { expect(subject).to have_many :answers }
-end
